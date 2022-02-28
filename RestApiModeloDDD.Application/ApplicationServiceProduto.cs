a7d8648da52a7665d0e333cf30cc3c51be6b831f
Gedeon Arruda <gd.deonnn@hotmail.com>
2022-02-28 11:58:25 -0300

    Add project files.

---
+++ RestApiModeloDDD.Application/ApplicationServiceProduto.cs
@@ -0,0 +1,44 @@
+﻿using RestApiModeloDDD.Application.Dtos;
+using RestApiModeloDDD.Application.Interfaces;
+using RestApiModeloDDD.Domain.Core.Interfaces.Services;
+using RestApiModeloDDD.Infrastructure.CrossCutting.Interfaces;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.Application
+{
+    public class ApplicationServiceProduto : IApplicationServiceProduto
+    {
+        private readonly IServiceProduto _serviceProduto;
+        private readonly IMapperProduto _mapperProduto;
+
+        public void Add(ProdutoDto produtoDto)
+        {
+            var produto = _mapperProduto.MapperDtoToEntity(produtoDto);
+            _serviceProduto.Add(produto);
+        }
+
+        public IEnumerable<ProdutoDto> GetAll()
+        {
+            var produtos = _serviceProduto.GetAll();
+            return _mapperProduto.MapperListProdutosDto(produtos);
+        }
+
+        public ProdutoDto GetById(int id)
+        {
+            var produto = _serviceProduto.GetById(id);
+            return _mapperProduto.MapperEntityToDto(produto);
+        }
+
+        public void Remove(ProdutoDto produtoDto)
+        {
+            var produto = _mapperProduto.MapperDtoToEntity(produtoDto);
+            _serviceProduto.Remove(produto);
+        }
+
+        public void Update(ProdutoDto produtoDto)
+        {
+            var produto = _mapperProduto.MapperDtoToEntity(produtoDto);
+            _serviceProduto.Update(produto);
+        }
+    }
+}
